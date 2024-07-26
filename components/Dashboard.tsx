a08ce529ca39026599898b53b85f5e61dc76f703
Mauricio Saez <saeztoro.mauricio@gmail.com>
2024-07-26 22:39:29 +0200

    Trying next auth. Using some guides..

---
+++ components/Dashboard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-800">
+            <div className="p-10 bg-gray-500 rounded shadow-md">
+                <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                <p className="text-gray-900">Welcome to your dashboard!</p>
+            </div>
+        </div>
+    );
+};
