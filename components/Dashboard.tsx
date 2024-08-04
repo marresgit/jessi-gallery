67368145c7bc62455071f70e255517b8515dbe1a
Mauricio Saez <saeztoro.mauricio@gmail.com>
2024-08-04 20:14:46 +0200

    Restrict login (simple way)

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {signIn, useSession} from "next-auth/react";
+import {signIn, signOut, useSession} from "next-auth/react";
 import React from "react";
 
 const Dashboard = () => {
@@ -10,11 +10,12 @@ const Dashboard = () => {
       <>
         {session ? (
             <>
-              <div className="flex items-center justify-center min-h-screen bg-gray-800">
-                <div className="p-10 bg-gray-500 rounded shadow-md">
-                  <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-                  <p className="text-gray-900">Welcome to your dashboard!</p>
-                </div>
+              <div className="p-10 bg-gray-500 rounded shadow-md">
+                <img src={session.user?.image} alt="user" className="w-20 h-20 rounded-full"/>
+                <h1 className="text-2xl font-semibold text-gray-900">Welcome {session.user?.name}</h1>
+
+                <p className="text-gray-900">{session.user?.email}</p>
+                <button onClick={() => signOut()} className="mt-5 bg-red-500 text-white px-3 py-1 rounded">Sign out</button>
               </div>
             </>
         ) : (
--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,4 +1,6 @@
 import { NextAuthOptions } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+
 
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -13,5 +15,17 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         })
-    ]
+    ],
+    callbacks:  {
+        async signIn({user }: {user: AdapterUser}) {
+            const allowedEmail = process.env.ALLOWED_EMAIL;
+            if (user.email === allowedEmail) {
+                return true; // Allow sign in
+                console.log(user.email);
+            } else {
+                console.log(user.email);
+                return false; // Do not allow sign in}
+            }
+        }
+    }
 }
